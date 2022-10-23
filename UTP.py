14968dbd31f3b6a7506e909281486c27aadb7fd9
Rivaldo <92324602+Rivaldo2117051054@users.noreply.github.com>
2022-10-23 11:02:38 +0700

    Update UTP.py

--- UTP.py
+++ UTP.py
@@ -1,2 +1,19 @@
 kaset = ["One Piece", "Naruto", "Dragon Ball", "Iron Man", "Ant Man", "Black Adam", "End Game"]
 sewa = [28000, 29000, 27000, 25000, 26000, 29000, 22000]
+
+loop = True
+
+while(loop):
+  
+  
+        print("\nApakah Anda Ingin Menyewa Lagi ?")
+        print("1. Ya\n2. Tidak")
+
+        pil = eval(input("Masukan Pilihan : "))
+
+        if pil == 2:
+            loop = False
+            print("\nProgram Dihentikan")
+            print("Terimakasih Telah Menggunakan Program Kami")
+        else:
+            print("\nProgram Di Ulang\n")
