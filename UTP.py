1085dbf8b4442564c0f101682ae509ac2992c7d9
Rivaldo <92324602+Rivaldo2117051054@users.noreply.github.com>
2022-10-23 11:10:16 +0700

    Update UTP.py

--- UTP.py
+++ UTP.py
@@ -16,10 +16,35 @@ loop = True
 
 while(loop):
     os.system('cls')
-    
-    
+        username = input("Masukan Nama Anda\t: ")
+    print("Nama Anda\t\t:", username.upper())
+    print("\nKaset Yang Tersedia\t: \n")
+
+    for i in range(0,7):
+        print(i + 1, "", kaset[i], "\t\tHarga Sewa :", sewa[i])
+
+    print()    
+
+    pilihan = eval(input("\nMasukan Kaset Yang Ingin Disewa\t: "))
+
     try:
-  
+        if pilihan == 1:
+            print("Anda Memilih\t\t:", kaset[pilihan-1])
+        elif pilihan == 2:
+            print("Anda Memilih\t\t:", kaset[pilihan-1])
+        elif pilihan == 3:
+            print("Anda Memilih\t\t:", kaset[pilihan-1])
+        elif pilihan == 4:
+            print("Anda Memilih\t\t:", kaset[pilihan-1])
+        elif pilihan == 5:
+            print("Anda Memilih\t\t:", kaset[pilihan-1])
+        elif pilihan == 6:
+            print("Anda Memilih\t\t:", kaset[pilihan-1])
+        elif pilihan == 7:
+            print("Anda Memilih\t\t:", kaset[pilihan-1])
+        else :
+            print(" Tidak ada")
+
   
         print("\nApakah Anda Ingin Menyewa Lagi ?")
         print("1. Ya\n2. Tidak")
